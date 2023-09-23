8142478716301064f99c830ef43773b2b842a090
Jesus <je.flores28@gmail.com>
2023-09-22 22:44:09 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+
+export default function Home() {
+  return (
+    <main>
+
+      {/* Header */}
+      <Header />
+      <h1>Trello AI Clone</h1>
+    </main>
+  )
+}
