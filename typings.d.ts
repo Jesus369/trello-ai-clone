362658ea68f878ed0cc1486f66d53b5e849a4e3a
Jesus <je.flores28@gmail.com>
2023-10-02 20:27:55 -0500

    correction

--- typings.d.ts
+++ typings.d.ts
@@ -1,23 +1,23 @@
 interface Board {
-    columns: Map<TypeColumn, Column>
+  columns: Map<TypedColumn, Column>;
 }
 
-type TypedColumn = "todo" | "inprogress" | "done"
+type TypedColumn = "todo" | "inprogress" | "done";
 
 interface Column {
-    id: TypedColumn;
-    todos: Todo[]
+  id: TypedColumn;
+  todos: Todo[];
 }
 
 interface Todo {
-    $id: string;
-    $createdAt: string;
-    title: string;
-    status: TypedColumn;
-    image?: Image;
+  $id: string;
+  $createdAt: string;
+  title: string;
+  status: TypedColumn;
+  image?: Image;
 }
 
 interface Image {
-    buckedIt: string;
-    fieldId: string;
+  buckedIt: string;
+  fieldId: string;
 }
